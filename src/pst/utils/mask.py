f725080d3169097ef748a51b9ec83913f07d03eb
cody-mar10 <ccmartin6@wisc.edu>
2023-05-01 09:58:07 -0500

    added fn to create a pairwise interaction mask for real/padded rows

--- src/pst/utils/mask.py
+++ src/pst/utils/mask.py
@@ -5,7 +5,7 @@ from typing import Optional
 import torch
 
 
-def compute_row_mask(X: torch.Tensor, unsqueeze: bool = False) -> torch.Tensor:
+def compute_row_mask(X: torch.Tensor) -> torch.Tensor:
     """Compute a row mask for input row-padded tensor.
 
     Args:
@@ -13,16 +13,29 @@ def compute_row_mask(X: torch.Tensor, unsqueeze: bool = False) -> torch.Tensor:
             b: batch size
             m: number of items
             d: feature dimension of item
-        unsqueeze(bool, optional): whether to unsqueeze the last dimension. Defaults to False.
 
     Returns:
         torch.Tensor: boolean tensor of shape [b, m, 1] that indicates which rows are
             real. True = real row. False = padded row of all 0s.
     """
     row_mask = X.abs().sum(-1) != 0.0
-    if unsqueeze:
-        return torch.unsqueeze(row_mask, dim=-1)
     return row_mask
+
+
+def pairwise_row_mask(
+    X_row_mask: torch.Tensor, Y_row_mask: torch.Tensor
+) -> torch.Tensor:
+    """Computes an pairwise interaction row_mask using logical operators.
+
+    Args:
+        X_row_mask (torch.Tensor): row_mask where True indicates real row, and
+            False indicates padded rows.
+        Y_row_mask (torch.Tensor): other row_mask
+
+    Returns:
+        torch.Tensor: pairwise row mask
+    """
+    return X_row_mask.unsqueeze(-1) & Y_row_mask.unsqueeze(-2)
 
 
 def row_mask_to_attn_mask(
@@ -34,11 +47,5 @@ def row_mask_to_attn_mask(
 
     # row masks have True for real rows and False for padded (all 0s) rows
     # but attn masks have True for positions where attn is not allowed
-    # and False for allowed positions
-    # so need to invert
-    attn_mask = (
-        torch.matmul(Q_row_mask.float(), K_row_mask.float().transpose(-2, -1))
-        .bool()
-        .logical_not()
-    )
-    return attn_mask
+    # and False for allowed positions so need to invert
+    return pairwise_row_mask(Q_row_mask, K_row_mask).logical_not()
