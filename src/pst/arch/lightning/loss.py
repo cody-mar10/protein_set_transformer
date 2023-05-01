cf238a86b2577d663b9c505966c2cfe9ea3e365e
cody-mar10 <ccmartin6@wisc.edu>
2023-05-01 10:16:51 -0500

    updated loss to be consistent with original impl
      - just needed to take max of diff of distances and 0 to bound the loss fn
      - also added a class weights option to rescale the loss for class imbalance

--- src/pst/arch/lightning/loss.py
+++ src/pst/arch/lightning/loss.py
@@ -10,6 +10,7 @@ class WeightedTripletLoss(nn.Module):
     def __init__(self, alpha: float) -> None:
         super(WeightedTripletLoss, self).__init__()
         self.alpha = alpha
+        self.loss_minimum = torch.tensor(0.0)
 
     def forward(
         self,
@@ -18,16 +19,23 @@ class WeightedTripletLoss(nn.Module):
         y_neg: torch.Tensor,
         weights: torch.Tensor,
         reduce: bool = False,
+        class_weights: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
-        # TODO: think this is computed wrong?
+        if class_weights is None:
+            # no actual weights
+            class_weights = torch.ones(y_self.size(0))
 
-        # positive_dist = torch.cdist(y_self, y_pos)
-        # negative_dist = torch.cdist(y_self, y_neg).mul(weights)
+        positive_dist = torch.pow(y_self - y_pos, 2).sum(dim=-1)
+        negative_dist = torch.pow(y_self - y_neg, 2).sum(dim=-1).mul(weights)
 
-        # This calcs the distance between vectors in same position, which is what is wanted
-        positive_dist = torch.pow(y_self - y_pos, 2).sum(dim=-1).sqrt()
-        negative_dist = torch.pow(y_self - y_neg, 2).sum(dim=-1).sqrt().mul(weights)
-        dist = positive_dist - negative_dist + self.alpha
+        # minimum dist is 0 for the perfect case
+        # class weights also rescale contribution to loss for a weighted average
+        # weights are 1.0 for most common class, and all rarer classes are > 1.0
+        # this has the effect of amplifying bad performance for rare classes
+        dist = (
+            torch.minimum(positive_dist - negative_dist + self.alpha, self.loss_minimum)
+            * class_weights
+        )
         if reduce:
             return torch.mean(dist, dim=0)
         return dist
@@ -45,17 +53,24 @@ class AugmentedWeightedTripletLoss(nn.Module):
         y_pos: torch.Tensor,
         y_neg: torch.Tensor,
         neg_weights: torch.Tensor,
+        class_weights: Optional[torch.Tensor] = None,
         y_aug_pos: Optional[torch.Tensor] = None,
         y_aug_neg: Optional[torch.Tensor] = None,
         aug_neg_weights: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
-        loss: torch.Tensor = self.loss_fn(y_self, y_pos, y_neg, neg_weights)
+        loss: torch.Tensor = self.loss_fn(
+            y_self, y_pos, y_neg, neg_weights, class_weights
+        )
 
         if y_aug_pos is not None:
             if y_aug_neg is None or aug_neg_weights is None:
                 raise ValueError("Should also provide y_aug_neg and aug_neg_weights")
             augmented_loss: torch.Tensor = self.loss_fn(
-                y_self, y_aug_pos, y_aug_neg, aug_neg_weights
+                y_self,
+                y_aug_pos,
+                y_aug_neg,
+                aug_neg_weights,
+                class_weights,
             )
             loss += augmented_loss
         return loss.mean(dim=0) / 2
