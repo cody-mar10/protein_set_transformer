b0e6de2066b7dfc06fbef71c15671a918fe8e0d0
cody-mar10 <ccmartin6@wisc.edu>
2023-06-21 12:41:57 -0500

    reorganized src dir and converting to graph impl

--- src/pst/arch/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import DeepSet, SetTransformer
-from ._blocks import AttentionSchema
-from ._norm import SetNorm
---
+++ src/pst/arch/modules.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import lightning as L
+
+
+class ProteinSetTransformer(L.LightningModule):
+    pass
