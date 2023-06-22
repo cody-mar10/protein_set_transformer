b3fb39758de3792922425b350e5cdbb4ebd089c0
cody-mar10 <ccmartin6@wisc.edu>
2023-06-22 15:46:57 -0500

    moved types to each submodule

--- src/pst/utils/_types.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from typing import Callable
-import torch
-
-FlowDict = dict[tuple[int, int], torch.Tensor]
-DistFuncSignature = Callable[[torch.Tensor, torch.Tensor], torch.Tensor]
-BatchType = tuple[torch.Tensor, torch.Tensor]
