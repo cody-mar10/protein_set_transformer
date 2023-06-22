7d605f37c2b30ed91ab1883878b0cd5709fd20e4
cody-mar10 <ccmartin6@wisc.edu>
2023-06-22 15:49:46 -0500

    added custom typing per submodule

---
+++ src/pst/arch/_types.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import Protocol
+
+from torch import Tensor
+from torch_geometric.typing import OptTensor
+
+
+class DataBatch(Protocol):
+    x: Tensor
+    y: OptTensor
+    edge_index: Tensor
+    batch: Tensor
+    ptr: Tensor
+    setsize: Tensor
+    weight: Tensor
+    class_id: Tensor
