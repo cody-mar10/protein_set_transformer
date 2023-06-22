294e8adcb6338a839cc772b26411b5ce6e29dfd1
cody-mar10 <ccmartin6@wisc.edu>
2023-06-22 16:27:27 -0500

    modified Trainer with consistent updates from cli arg changes

--- src/pst/training/train.py
+++ src/pst/training/train.py
@@ -24,10 +24,15 @@ class Trainer:
         optimizer_kwargs: dict[str, Any],
         data_kwargs: dict[str, Any],
         trainer_kwargs: dict[str, Any],
+        experiment_kwargs: dict[str, Any],
+        loss_kwargs: dict[str, Any],
+        augmentation_kwargs: dict[str, Any],
     ) -> None:
-        self.model_kwargs = model_kwargs | optimizer_kwargs
+        self.model_kwargs = model_kwargs | optimizer_kwargs | augmentation_kwargs
         self.data_kwargs = data_kwargs
         self.trainer_kwargs = trainer_kwargs
+        self.experiment_kwargs = experiment_kwargs
+        self.loss_kwargs = loss_kwargs
 
     @classmethod
     def from_cli_args(cls, args: Args):
@@ -36,25 +41,35 @@ class Trainer:
             optimizer_kwargs=args.optimizer,
             data_kwargs=args.data,
             trainer_kwargs=args.trainer,
+            experiment_kwargs=args.experiment,
+            loss_kwargs=args.loss,
+            augmentation_kwargs=args.augmentation,
         )
 
+    # TODO: make configurable from cli
     def trainer_callbacks(self) -> list[L.Callback]:
-        checkpointing = ModelCheckpoint(
-            monitor="val_loss", save_top_k=10, every_n_epochs=1
+        callbacks: list[L.Callback] = list()
+
+        callbacks.append(
+            ModelCheckpoint(monitor="val_loss", save_top_k=10, every_n_epochs=1)
         )
-        early_stopping = EarlyStopping(
-            monitor="val_loss",
-            min_delta=0.0,
-            patience=self.trainer_kwargs["patience"],  # TODO: add and pop?
+        callbacks.append(
+            EarlyStopping(
+                monitor="val_loss",
+                min_delta=0.0,
+                patience=self.experiment_kwargs["patience"],
+            )
         )
-        lr_monitor = LearningRateMonitor(logging_interval="step")
-        swa = StochasticWeightAveraging(
-            swa_lrs=self.model_kwargs["lr"],
-            swa_epoch_start=0.75,
-            annealing_strategy="linear",
+        callbacks.append(LearningRateMonitor(logging_interval="step"))
+        callbacks.append(
+            StochasticWeightAveraging(
+                swa_lrs=self.model_kwargs["lr"],
+                swa_epoch_start=0.75,
+                annealing_strategy="linear",
+            )
         )
 
-        return [checkpointing, lr_monitor, early_stopping, swa]
+        return callbacks
 
     def train(
         self,
@@ -79,18 +94,14 @@ class Trainer:
 
             logger = TensorBoardLogger(
                 save_dir=self.trainer_kwargs["default_root_dir"],
-                name=self.trainer_kwargs[
-                    "experiment_name"
-                ],  # TODO: add, and it will need to be popped?
+                name=self.experiment_kwargs["name"],
                 version=fold_idx,
                 default_hp_metric=False,
             )
             trainer = L.Trainer(
                 callbacks=self.trainer_callbacks(),
                 logger=logger,
-                log_every_n_steps=5,
-                gradient_clip_val=0.5,
-                gradient_clip_algorithm="norm",
+                log_every_n_steps=1,
                 num_sanity_val_steps=0,
                 **self.trainer_kwargs,
             )
@@ -106,13 +117,11 @@ class Trainer:
     def summarize_cross_validation(self):
         cv_summarizer = CrossValEventSummarizer(
             root_dir=self.trainer_kwargs["default_root_dir"],
-            name=self.trainer_kwargs["experiment_name"],
+            name=self.experiment_kwargs["name"],
         )
         # TODO: add in args
-        # TODO: actually summarize this
-        # each training fold will not have the same number of steps...so how to do this? maybe just normalize step #?
         cv_summarizer.summarize_and_save(
-            output_name="cross_validation_metrics.tsv",
+            output_name="cross_validation_summary.tsv",
             metric="val_loss",
             step="epoch",
         )
