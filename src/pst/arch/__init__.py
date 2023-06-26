3fa251c2d1f04cb68067c532d9f1040a381e2bd2
cody-mar10 <ccmartin6@wisc.edu>
2023-06-26 15:25:11 -0500

    make `EdgeIndexStrategy` public

--- src/pst/arch/__init__.py
+++ src/pst/arch/__init__.py
@@ -1,3 +1,3 @@
-from .data import GenomeDataset, GenomeDataModule
-from .modules import ProteinSetTransformer
-from .training import distance, loss, sampling
+from .data import EdgeIndexStrategy, GenomeDataModule, GenomeDataset  # noqa: F401
+from .modules import ProteinSetTransformer  # noqa: F401
+from .training import distance, loss, sampling  # noqa: F401
