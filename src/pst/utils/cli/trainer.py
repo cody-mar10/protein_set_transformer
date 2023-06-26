ce6912b6fb908f68cb4f60ee855273e634130395
cody-mar10 <ccmartin6@wisc.edu>
2023-06-26 16:08:15 -0500

    made opts public and added max time option

--- src/pst/utils/cli/trainer.py
+++ src/pst/utils/cli/trainer.py
@@ -2,27 +2,59 @@ from __future__ import annotations
 
 import argparse
 from dataclasses import dataclass
+from datetime import timedelta
 from pathlib import Path
 from typing import Literal, Optional
 
 from .utils import asdict, register_defaults, register_handler
+
+AcceleratorOpts = Literal["cpu", "gpu", "tpu", "auto"]
+PrecisionOpts = Literal["16-mixed", "bf16-mixed", 32, "32"]
+StrategyOpts = Literal["ddp", "ddp_spawn", "ddp_notebook", "fsdp"]
+GradClipAlgOpts = Literal["norm", "value"]
+MaxTimeOpts = Literal["short", "medium", "long", None]
 
 
 # these are directly passed to the lightning.Trainer class as kwargs
 @dataclass
 class TrainerArgs:
     devices: int = 1
-    accelerator: Literal["cpu", "gpu", "tpu", "auto"] = "gpu"
+    accelerator: AcceleratorOpts = "gpu"
     default_root_dir: Path = Path("lightning_root")
     max_epochs: int = 1000
-    precision: Literal["16-mixed", "bf16-mixed", 32, "32"] = "16-mixed"
-    strategy: Literal["ddp", "ddp_spawn", "ddp_notebook"] = "ddp"
-    gradient_clip_algorithm: Optional[Literal["norm", "value"]] = None
+    precision: PrecisionOpts = "16-mixed"
+    strategy: StrategyOpts = "ddp"
+    gradient_clip_algorithm: Optional[GradClipAlgOpts] = None
     gradient_clip_val: Optional[float] = None
+    max_time: Optional[timedelta] = None
 
 
 _DEFAULTS = TrainerArgs()
 register_defaults(_DEFAULTS, "trainer")
+
+
+def _convert_max_time(
+    val: MaxTimeOpts,
+    buffer_amount: int = 15,
+    buffer_type: Literal["days", "hours", "minutes"] = "minutes",
+) -> Optional[timedelta]:
+    # these are based on CHTC guidelines
+
+    # add a small buffer to stop training and save outputs in chtc
+    buffer_kwargs = {buffer_type: buffer_amount}
+    buffer = timedelta(**buffer_kwargs)
+    match val:
+        case "short":
+            limit = timedelta(hours=12) - buffer
+            return limit
+        case "medium":
+            limit = timedelta(days=1) - buffer
+            return limit
+        case "long":
+            limit = timedelta(weeks=1) - buffer
+            return limit
+        case _:
+            return None
 
 
 @register_handler
@@ -92,11 +124,21 @@ def add_trainer_args(parser: argparse.ArgumentParser):
         type=float,
         help="optional value used if clipping gradients (default: %(default)s)",
     )
+    group.add_argument(
+        "--max-time",
+        metavar="",
+        choices={"short", "medium", "long", None, "None"},
+        default=_DEFAULTS.max_time,
+        help=(
+            "maximum amount of time for training (default: %(default)s) "
+            "[choices: short=12h, medium=1d, long=7d, None=no limit]"
+        ),
+    )
 
 
 @asdict
 def parse_trainer_args(args: argparse.Namespace) -> TrainerArgs:
-    return TrainerArgs(
+    targs = TrainerArgs(
         devices=args.devices,
         accelerator=args.accelerator,
         default_root_dir=args.default_root_dir,
@@ -105,4 +147,7 @@ def parse_trainer_args(args: argparse.Namespace) -> TrainerArgs:
         strategy=args.strategy,
         gradient_clip_algorithm=args.gradient_clip_algorithm,
         gradient_clip_val=args.gradient_clip_val,
+        max_time=_convert_max_time(args.max_time),
     )
+
+    return targs
