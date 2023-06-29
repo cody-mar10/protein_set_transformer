5d45411aeb08661344dee5102f46e3dc826d781d
cody-mar10 <ccmartin6@wisc.edu>
2023-06-29 15:30:49 -0500

    added fn to parse int/float rags   - added fn to optionall validate int/float args

--- src/pst/utils/cli/utils.py
+++ src/pst/utils/cli/utils.py
@@ -4,7 +4,7 @@ import argparse
 from dataclasses import asdict as dc_asdict
 from dataclasses import is_dataclass
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any, Callable, Optional, TypeVar
 
 _ADDER_TYPE = Callable[[argparse.ArgumentParser], None]
 _ARGPARSE_HANDLERS: list[_ADDER_TYPE] = list()
@@ -40,3 +40,45 @@ def asdict(fn: _PARSER_TYPE):
         )
 
     return wrap
+
+
+_T = TypeVar("_T")
+
+
+def _validate_and_call_predicate(
+    x: str, val: _T, predicate: Optional[Callable[[_T], bool]] = None
+):
+    if predicate is not None:
+        if not predicate(val):
+            raise ValueError(
+                f"Failed validation check when parsing CLI args: {x} -> {val}"
+            )
+
+
+def parse_int_or_float_arg(
+    x: str,
+    int_predicate: Optional[Callable[[int], bool]] = None,
+    float_predicate: Optional[Callable[[float], bool]] = None,
+) -> int | float:
+    if x.isdigit():
+        val = int(x)
+        _validate_and_call_predicate(x, val, int_predicate)
+        return val
+
+    val = float(x)
+    _validate_and_call_predicate(x, val, float_predicate)
+    return val
+
+
+def validate_proportion_range(
+    x: float, left_inclusive: bool = True, right_inclusive: bool = True
+) -> bool:
+    low = 0.0
+    high = 1.0
+    if left_inclusive and right_inclusive:
+        return low <= x <= high
+    elif left_inclusive:
+        return low <= x < high
+    elif right_inclusive:
+        return low < x <= high
+    return low < x < high
