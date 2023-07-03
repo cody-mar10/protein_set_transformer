2d5042e44dfd88943953b921c7adf73ae1d1bbe3
cody-mar10 <ccmartin6@wisc.edu>
2023-07-03 16:52:56 -0500

    need to check python 3.11 for toml lib

--- src/pst/utils/versions.py
+++ src/pst/utils/versions.py
@@ -1,3 +1,5 @@
+from platform import python_version
+
 import lightning
 import torch
 from packaging import version
@@ -6,3 +8,5 @@ _VERSION_2 = version.parse("2")
 
 TORCH_VERSION_GTE_2 = version.parse(torch.__version__) >= _VERSION_2
 LIGHTNING_VERSION_GTE_2 = version.parse(lightning.__version__) >= _VERSION_2
+
+PYTHON_VERSION_GTE_3_11 = version.parse(python_version()) >= version.parse("3.11")
