7fa492aa03c8b7d8bec2e7b5ac5d2a41b87b1d54
cody-mar10 <ccmartin6@wisc.edu>
2023-07-04 10:03:59 -0500

    added default config

--- src/pst/training/config.py
+++ src/pst/training/config.py
@@ -65,3 +65,55 @@ def load_config(file: Path) -> dict[str, dict[str, dict[str, Any]]]:
         raise e
 
     return validated_config
+
+
+_DEFAULT_CONFIG = {
+    "model": {
+        "complexity": {
+            "suggest": "categorical",
+            "choices": ["small", "medium", "large", "xl", "xxl"],
+            "map": {
+                "small": {"num_heads": 4, "n_enc_layers": 5},
+                "medium": {"num_heads": 8, "n_enc_layers": 10},
+                "large": {"num_heads": 16, "n_enc_layers": 15},
+                "xl": {"num_heads": 32, "n_enc_layers": 20},
+                "xxl": {"num_heads": 32, "n_enc_layers": 30},
+            },
+        },
+        "multiplier": {"suggest": "float", "low": 0.5, "high": 10.0, "log": True},
+        "dropout": {"suggest": "float", "low": 0.0, "high": 0.5, "log": False},
+    },
+    "optim": {
+        "lr": {"suggest": "float", "low": 0.001, "high": 0.1, "log": True},
+        "weight_decay": {"suggest": "float", "low": 0.001, "high": 0.1, "log": True},
+    },
+    "data": {
+        "batch_size": {"suggest": "categorical", "choices": [16, 32, 64]},
+        "chunk_size": {
+            "suggest": "int",
+            "low": 15,
+            "high": 50,
+            "log": False,
+            "step": 5,
+        },
+    },
+    "loss": {"margin": {"suggest": "float", "low": 0.1, "high": 10.0, "log": True}},
+    "experiment": {
+        "swa_epoch_start": {"suggest": "float", "low": 0.5, "high": 0.8, "log": False},
+        "annealing_epochs": {
+            "suggest": "int",
+            "low": 10,
+            "high": 100,
+            "log": False,
+            "step": 1,
+        },
+        "annealing_strategy": {
+            "suggest": "categorical",
+            "choices": ["linear", "cosine"],
+        },
+    },
+    "augmentation": {
+        "sample_scale": {"suggest": "float", "low": 1.0, "high": 15.0, "log": False},
+        "sample_rate": {"suggest": "float", "low": 0.25, "high": 0.75, "log": False},
+    },
+}
