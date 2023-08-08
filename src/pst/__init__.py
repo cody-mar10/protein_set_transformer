b7fbd4f6d7f7d47cdd5b121315dd85726bdd1b72
cody-mar10 <ccmartin6@wisc.edu>
2023-08-08 10:18:28 -0500

    removed training/cv/tuning exports and `noqa`

--- src/pst/__init__.py
+++ src/pst/__init__.py
@@ -1,7 +1,3 @@
-from . import _typing, arch, cross_validation, model, training, utils  # noqa: F401
-from .predict import Predictor  # noqa: F401
-from .training import (  # noqa: F401
-    CrossValidationTrainer,
-    FullTrainer,
-    TuningCrossValidationTrainer,
-)
+from . import _typing, arch, cross_validation, model, training, utils
+from .arch import CrossValPST, ProteinSetTransformer
+from .predict import Predictor
