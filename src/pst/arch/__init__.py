7c49e0de500d57cf75b6f0a8b17ed95e5f0c0d1e
cody-mar10 <ccmartin6@wisc.edu>
2023-08-08 09:55:02 -0500

    export cross val version of model
      - also remove noqa

--- src/pst/arch/__init__.py
+++ src/pst/arch/__init__.py
@@ -1,3 +1,3 @@
-from .data import EdgeIndexStrategy, GenomeDataModule, GenomeDataset  # noqa: F401
-from .modules import ProteinSetTransformer  # noqa: F401
-from .training import distance, loss, sampling  # noqa: F401
+from .data import EdgeIndexStrategy, GenomeDataModule, GenomeDataset
+from .modules import CrossValPST, ProteinSetTransformer
+from .training import distance, loss, sampling
