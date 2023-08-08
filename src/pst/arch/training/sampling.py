70b6bca13bcefdd43182da699a52852b904513f6
cody-mar10 <ccmartin6@wisc.edu>
2023-08-08 09:52:47 -0500

    put pydantic models into same file for simplicity

--- src/pst/arch/training/sampling.py
+++ src/pst/arch/training/sampling.py
@@ -1,13 +1,30 @@
 from __future__ import annotations
 
-from typing import Optional
+from typing import Literal, Optional
 
 import torch
+from pydantic import BaseModel, Field
 
 from pst._typing import FlowType
-from pst.utils.cli import NO_NEGATIVES_MODES
 
 from .distance import pairwise_chamfer_distance
+
+NO_NEGATIVES_MODES = Literal["closest_to_positive", "closest_to_anchor"]
+
+
+class AugmentationConfig(BaseModel):
+    sample_scale: float = Field(
+        7.0,
+        description="exponential decay scale factor for weighting negative samples during loss",  # noqa: E501
+        gt=0.0,
+    )
+    sample_rate: float = Field(
+        0.5, description="PointSwap sampler swapping rate", gt=0.0, lt=1.0
+    )
+    no_negatives_mode: NO_NEGATIVES_MODES = Field(
+        "closest_to_positive",
+        description="mode to handle event of no semihard negative sample existing",
+    )
 
 
 def positive_sampling(
