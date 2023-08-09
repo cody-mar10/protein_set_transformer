d5ebe2d826240a451cb8b04c72260ba69e153061
cody-mar10 <ccmartin6@wisc.edu>
2023-08-09 18:16:09 -0500

    update with `lightning_cv.Tuner` updates
     - also use a peeking fn to get feature dim

--- src/pst/training/tuning.py
+++ src/pst/training/tuning.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
 from functools import partial
+from pathlib import Path
 
 import lightning_cv as lcv
+import numpy as np
 import optuna
+import tables as tb
 
 from pst.arch import GenomeDataModule
 from pst.arch.modules import CrossValPST as PST
@@ -12,6 +15,11 @@ from pst.utils.cli import TrainingMode
 
 def _optimize(trial: optuna.Trial, tuner: lcv.tuning.Tuner) -> float:
     return tuner.tune(trial=trial)
+
+
+def _peek_feature_dim(data_file: Path) -> int:
+    with tb.File(data_file) as fp:
+        return np.shape(fp.root.data[0])[-1]
 
 
 def tune(config: TrainingMode):
@@ -24,11 +32,9 @@ def tune(config: TrainingMode):
         else optuna.pruners.NopPruner()
     )
 
-    datamodule = GenomeDataModule(config.data)
-
     # update model's in_dim
     if config.model.in_dim == -1:
-        config.model.in_dim = datamodule.dataset.feature_dim
+        config.model.in_dim = _peek_feature_dim(config.data.file)
 
     trainer_callbacks: list[lcv.callbacks.Callback] = [
         lcv.callbacks.ModelCheckpoint(
@@ -54,10 +60,11 @@ def tune(config: TrainingMode):
     tuner = lcv.tuning.Tuner(
         model_type=PST,  # type: ignore
         model_config=config.model,
+        datamodule_type=GenomeDataModule,
+        datamodule_config=config.data,
         trainer_config=trainer_config,
         logdir=config.trainer.default_root_dir,
         experiment_name=config.experiment.name,
-        datamodule=datamodule,
         hparam_config_file=config.experiment.config,
     )
 
@@ -76,5 +83,8 @@ def tune(config: TrainingMode):
         )
     ]
     study.optimize(
-        func=optimize, n_trials=config.experiment.n_trials, callbacks=optuna_callbacks
+        func=optimize,
+        n_trials=config.experiment.n_trials,
+        callbacks=optuna_callbacks,
+        gc_after_trial=True,
     )
