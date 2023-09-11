360246ba4cfc268368d4a52d2b2e813652cd6cce
cody-mar10 <ccmartin6@wisc.edu>
2023-09-11 12:52:56 -0500

    put cli arg parsing here and removed `cli.py`

--- src/pst/utils/cli/__init__.py
+++ src/pst/utils/cli/__init__.py
@@ -1,8 +1,26 @@
-from .cli import Args, InferenceMode, TrainingMode, parse_args
-from .experiment import AnnealingOpts
-from .trainer import (
-    AcceleratorOpts,
-    GradClipAlgOpts,
-    PrecisionOpts,
-    StrategyOpts,
-)
+from __future__ import annotations
+
+from typing import Optional
+
+import pydantic_argparse
+from pydantic import BaseModel, Field
+
+from .modes import InferenceMode, TrainingMode, TuningMode
+
+
+class Args(BaseModel):
+    train: Optional[TrainingMode] = Field(None, description="train mode")
+    tune: Optional[TuningMode] = Field(None, description="tune mode")
+    predict: Optional[InferenceMode] = Field(None, description="predict/inference mode")
+
+
+def parse_args(args: Optional[list[str]] = None) -> Args:
+    parser = pydantic_argparse.ArgumentParser(
+        model=Args,
+        description=(
+            "Train or predict genome-level embeddings based on sets of protein-level "
+            "embeddings"
+        ),
+    )
+
+    return parser.parse_typed_args(args)
