2c4094ff28b327ec997b0ddc7e2137a5124ffd99
cody-mar10 <ccmartin6@wisc.edu>
2023-09-13 10:31:20 -0500

    use maxtime value instead of enum   - also add better model checkpointing callback

--- src/pst/training/full.py
+++ src/pst/training/full.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint
 
 from pst.arch.data import GenomeDataModule
 from pst.arch.modules import ProteinSetTransformer as PST
@@ -20,5 +21,14 @@ def train_with_all_data(config: TrainingMode):
 
     model = PST(config.model)
 
-    trainer = L.Trainer(**config.trainer.model_dump())
+    checkpoint_callback = ModelCheckpoint(
+        save_last=True, save_top_k=config.experiment.save_top_k, every_n_epochs=1
+    )
+
+    # want max time value, not enum
+    trainer = L.Trainer(
+        max_time=config.trainer.max_time.value,
+        callbacks=[checkpoint_callback],
+        **config.trainer.model_dump(exclude={"max_time"}),
+    )
     trainer.fit(model=model, datamodule=datamodule)
