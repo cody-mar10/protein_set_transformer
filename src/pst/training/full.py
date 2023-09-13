d761db4708feeb32e073d0b0a258937b44755723
cody-mar10 <ccmartin6@wisc.edu>
2023-09-13 16:13:28 -0500

    add early stopping callback for finite loss   - also checks improvement thresholds   - mirrors settings during tuning   - checkpoints now contain train loss in name

--- src/pst/training/full.py
+++ src/pst/training/full.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 import lightning as L
-from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.callbacks import Callback, EarlyStopping, ModelCheckpoint
 
 from pst.arch.data import GenomeDataModule
 from pst.arch.modules import ProteinSetTransformer as PST
 from pst.utils.cli.modes import TrainingMode
+
+# TODO early stopp
 
 
 def train_with_all_data(config: TrainingMode):
@@ -21,17 +23,31 @@ def train_with_all_data(config: TrainingMode):
 
     model = PST(config.model)
 
-    checkpoint_callback = ModelCheckpoint(
-        save_last=True,
-        save_top_k=config.experiment.save_top_k,
-        every_n_epochs=1,
-        monitor="train_loss",
-    )
+    callbacks: list[Callback] = [
+        EarlyStopping(
+            monitor="train_loss",
+            patience=config.experiment.patience,
+            verbose=True,
+            mode="min",
+            check_finite=True,
+            strict=True,
+            stopping_threshold=1e-3,
+            min_delta=0.05,
+        ),
+        ModelCheckpoint(
+            filename="{epoch}_{train_loss:.3f}",
+            save_last=True,
+            save_top_k=config.experiment.save_top_k,
+            every_n_epochs=1,
+            monitor="train_loss",
+            mode="min",
+        ),
+    ]
 
-    # want max time value, not enum
     trainer = L.Trainer(
+        # want max time value, not enum
         max_time=config.trainer.max_time.value,
-        callbacks=[checkpoint_callback],
+        callbacks=callbacks,
         **config.trainer.model_dump(exclude={"max_time"}),
     )
     trainer.fit(model=model, datamodule=datamodule)
