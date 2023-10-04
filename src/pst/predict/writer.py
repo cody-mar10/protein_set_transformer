dcdca562241d0744b3b972d748444a01ee292500
cody-mar10 <ccmartin6@wisc.edu>
2023-10-04 16:22:17 -0500

    update to handle decoder tuple output   - predict step always returns both data and attn tensors   - can save both to the same `.h5` file

--- src/pst/predict/writer.py
+++ src/pst/predict/writer.py
@@ -9,10 +9,10 @@ import numpy as np
 import tables as tb
 import torch
 from lightning.pytorch.callbacks import BasePredictionWriter
-from numpy.typing import NDArray
 
 from pst.data.modules import GenomeDataModule
 from pst.nn.modules import ProteinSetTransformer
+from pst.typing import NumpyFloat32, PairTensor
 
 
 def sort_key(file: Path) -> int:
@@ -42,7 +42,7 @@ class PredictionWriter(BasePredictionWriter):
         self,
         trainer: L.Trainer,
         pl_module: ProteinSetTransformer,
-        predictions: torch.Tensor,
+        predictions: PairTensor,
         batch_indices: Optional[list[int]],
         batch: torch.Tensor,
         batch_idx: int,
@@ -52,16 +52,37 @@ class PredictionWriter(BasePredictionWriter):
         outdir = self.outdir.joinpath(f"{self.dataset_prefix}_{dataloader_idx}")
         outdir.mkdir(parents=True, exist_ok=True)
         output_file = outdir.joinpath(f"{self.batch_prefix}_{batch_idx}.h5")
-        predictions = predictions.cpu().numpy()
+
+        data, attn = predictions
+        np_data: NumpyFloat32 = data.cpu().numpy()
+        np_attn: NumpyFloat32 = attn.cpu().numpy()
 
         with tb.File(output_file, "w") as fp:
             fp.create_carray(
                 "/",
                 "data",
-                obj=predictions,
-                shape=predictions.shape,
+                obj=np_data,
+                shape=np_data.shape,
                 filters=self.compression,
             )
+
+            fp.create_carray(
+                "/",
+                "attn",
+                obj=np_attn,
+                shape=np_attn.shape,
+                filters=self.compression,
+            )
+
+    def _concat_data(self, fdst: tb.File, data: list[NumpyFloat32], name: str):
+        concat = np.vstack(data)
+        fdst.create_carray(
+            "/",
+            name,
+            obj=concat,
+            shape=concat.shape,
+            filters=self.compression,
+        )
 
     def on_predict_end(self, trainer: L.Trainer, pl_module: L.LightningModule) -> None:
         """Combine all separate batch files into a single file
@@ -85,20 +106,17 @@ class PredictionWriter(BasePredictionWriter):
         # copy .h5 embeddings arrays
         for dataset_path, batch_paths in data_paths.items():
             data_output = self.outdir.joinpath(f"{dataset_path.name}.h5")
-            data: list[NDArray[np.float32]] = list()
+
+            data: list[NumpyFloat32] = list()
+            attn: list[NumpyFloat32] = list()
             with tb.File(data_output, "w") as fdst:
                 for data_path in batch_paths:
                     with tb.File(data_path) as fsrc:
                         data.append(fsrc.root.data[:])
+                        attn.append(fsrc.root.attn[:])
 
-                concat = np.vstack(data)
-                fdst.create_carray(
-                    "/",
-                    "data",
-                    obj=concat,
-                    shape=concat.shape,
-                    filters=self.compression,
-                )
+                self._concat_data(fdst=fdst, data=data, name="data")
+                self._concat_data(fdst=fdst, data=attn, name="attn")
 
         # delete dataset files
         for dataset_path in dataset_paths:
