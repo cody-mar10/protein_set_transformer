af6af629ade2904f5bddf6192fe7b637e08b4191
cody-mar10 <ccmartin6@wisc.edu>
2023-10-09 00:39:03 -0500

    allow checking gradient anomalies at toplevel

--- src/pst/main.py
+++ src/pst/main.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+import logging
+
 import lightning as L
 import torch
+from torch.autograd.anomaly_mode import set_detect_anomaly
 
 from pst.predict import model_inference
 from pst.training import cv, full
@@ -11,6 +14,7 @@ from pst.utils.cli.modes import InferenceMode, TrainingMode, TuningMode
 from pst.utils.history import update_config_from_history
 
 _SEED = 111
+logger = logging.getLogger(__name__)
 
 
 def train_main(args: TrainingMode):
@@ -50,17 +54,29 @@ def main():
     if args.train is not None:
         config = args.train
         _check_cpu_accelerator(config)
-        train_main(config)
+        fn = train_main
     elif args.tune is not None:
         config = args.tune
         _check_cpu_accelerator(config)
-        tune_main(config)
+        fn = tune_main
     elif args.predict is not None:
         config = args.predict
         _check_cpu_accelerator(config)
-        predict_main(config)
+        fn = predict_main
     else:
-        raise RuntimeError("Must pass either 'train' or 'predict' as running mode.")
+        raise RuntimeError(
+            "Must pass either 'train', 'tune', or 'predict' as running mode."
+        )
+
+    with set_detect_anomaly(config.experiment.debug):
+        if config.experiment.debug:
+            logger.warning(
+                "Debug mode is on. This will be slow since the autograd engine has to "
+                "check for gradient anomalies. Additionally, the gradients will be "
+                "written to disk, which may take up a lot of space."
+            )
+
+        fn(config)  # type: ignore
 
 
 if __name__ == "__main__":
