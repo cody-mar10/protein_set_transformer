00d49c91450d72017157cb0f7742147882cb1ec5
cody-mar10 <ccmartin6@wisc.edu>
2023-10-11 19:00:02 -0500

    use expandable arrays to write outputs per batch

--- src/pst/predict/writer.py
+++ src/pst/predict/writer.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
+import logging
 from pathlib import Path
-from shutil import rmtree
-from typing import Literal, Optional
+from typing import Literal, Optional, Sequence
 
 import lightning as L
-import numpy as np
 import tables as tb
 import torch
 from lightning.pytorch.callbacks import BasePredictionWriter
@@ -13,6 +12,8 @@ from lightning.pytorch.callbacks import BasePredictionWriter
 from pst.data.modules import GenomeDataModule
 from pst.nn.modules import ProteinSetTransformer
 from pst.typing import NumpyFloat32, PairTensor
+
+logger = logging.getLogger(__name__)
 
 
 def sort_key(file: Path) -> int:
@@ -25,99 +26,78 @@ def sort_key(file: Path) -> int:
 
 
 class PredictionWriter(BasePredictionWriter):
+    OUTPUTS = ["data", "attn"]
+
     def __init__(
         self,
         outdir: Path,
+        model: ProteinSetTransformer,
         datamodule: GenomeDataModule,
+        output_name: str = "predictions.h5",
         write_interval: Literal["batch", "epoch", "batch_and_epoch"] = "batch",
     ) -> None:
         super().__init__(write_interval)
         self.outdir = outdir
         self.datamodule = datamodule
+
         self.compression = tb.Filters(complevel=4, complib="blosc:lz4")
-        self.dataset_prefix = "dataset"
-        self.batch_prefix = "batch"
+        self._file = tb.File(self.outdir.joinpath(output_name), "w")
+        self._arrays: dict[str, tb.EArray] = dict()
+
+        n_nodes = self.datamodule.dataset.data.shape[0]
+        n_graphs = int(self.datamodule.dataset.sizes.numel())
+        for output in PredictionWriter.OUTPUTS:
+            if output == "data":
+                n = n_graphs
+                shape = (0, model.config.out_dim)
+            else:
+                n = n_nodes
+                shape = (0, model.config.num_heads)
+
+            array = self._file.create_earray(
+                self._file.root,
+                output,
+                atom=tb.Float32Atom(),
+                shape=shape,
+                expectedrows=n,
+                filters=self.compression,
+            )
+
+            self._arrays[output] = array
 
     def write_on_batch_end(
         self,
         trainer: L.Trainer,
-        pl_module: ProteinSetTransformer,
+        pl_module: L.LightningModule,
         predictions: PairTensor,
-        batch_indices: Optional[list[int]],
+        batch_indices: Optional[Sequence[int]],
         batch: torch.Tensor,
         batch_idx: int,
         dataloader_idx: int,
     ):
-        # results should come in order
-        outdir = self.outdir.joinpath(f"{self.dataset_prefix}_{dataloader_idx}")
-        outdir.mkdir(parents=True, exist_ok=True)
-        output_file = outdir.joinpath(f"{self.batch_prefix}_{batch_idx}.h5")
-
+        # results should come in the same order as the data is stored in the dataset
         data, attn = predictions
         np_data: NumpyFloat32 = data.cpu().numpy()
         np_attn: NumpyFloat32 = attn.cpu().numpy()
 
-        with tb.File(output_file, "w") as fp:
-            fp.create_carray(
-                "/",
-                "data",
-                obj=np_data,
-                shape=np_data.shape,
-                filters=self.compression,
-            )
-
-            fp.create_carray(
-                "/",
-                "attn",
-                obj=np_attn,
-                shape=np_attn.shape,
-                filters=self.compression,
-            )
-
-    def _concat_data(self, fdst: tb.File, data: list[NumpyFloat32], name: str):
-        concat = np.vstack(data)
-        fdst.create_carray(
-            "/",
-            name,
-            obj=concat,
-            shape=concat.shape,
-            filters=self.compression,
-        )
+        for name, array in zip(PredictionWriter.OUTPUTS, [np_data, np_attn]):
+            self._arrays[name].append(array)
 
     def on_predict_end(self, trainer: L.Trainer, pl_module: L.LightningModule) -> None:
-        """Combine all separate batch files into a single file
-
-        Args:
-            trainer (pl.Trainer): pytorch-lightning trainer
-            pl_module (pl.LightningModule): pytorch-lightning module
-        """
-        super().on_predict_end(trainer, pl_module)
-
-        dataset_paths = sorted(
-            self.outdir.glob(f"*{self.dataset_prefix}*/"), key=sort_key
-        )
-        data_paths = {
-            dataset_path: sorted(
-                dataset_path.glob(f"*{self.batch_prefix}*.h5"), key=sort_key
+        num_graphs = self._arrays["data"].shape[0]  # type: ignore
+        if num_graphs != int(self.datamodule.dataset.sizes.numel()):
+            raise RuntimeError(
+                f"Expected {self.datamodule.dataset.sizes.numel()} graphs in output but "
+                f"got {num_graphs}"
             )
-            for dataset_path in dataset_paths
-        }
 
-        # copy .h5 embeddings arrays
-        for dataset_path, batch_paths in data_paths.items():
-            data_output = self.outdir.joinpath(f"{dataset_path.name}.h5")
+        num_nodes = self._arrays["attn"].shape[0]  # type: ignore
+        if num_nodes != int(self.datamodule.dataset.data.shape[0]):
+            raise RuntimeError(
+                f"Expected {self.datamodule.dataset.data.shape[0]} nodes in output but "
+                f"got {num_nodes}"
+            )
 
-            data: list[NumpyFloat32] = list()
-            attn: list[NumpyFloat32] = list()
-            with tb.File(data_output, "w") as fdst:
-                for data_path in batch_paths:
-                    with tb.File(data_path) as fsrc:
-                        data.append(fsrc.root.data[:])
-                        attn.append(fsrc.root.attn[:])
+        self._file.close()
 
-                self._concat_data(fdst=fdst, data=data, name="data")
-                self._concat_data(fdst=fdst, data=attn, name="attn")
-
-        # delete dataset files
-        for dataset_path in dataset_paths:
-            rmtree(dataset_path)
+        logger.info(f"Saved predictions to {self.outdir}")
