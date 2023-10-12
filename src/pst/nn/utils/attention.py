fa48d1ff3eb9099deb5c28b32b2a34a351916675
cody-mar10 <ccmartin6@wisc.edu>
2023-10-12 11:39:34 -0500

    convert attn utils to a mixin class

--- src/pst/nn/utils/attention.py
+++ src/pst/nn/utils/attention.py
@@ -1,16 +1,32 @@
+from __future__ import annotations
+
 import math
 
 from einops import rearrange
 from torch import Tensor
+from torch.nn import Linear
 
 
-def attention_scale(dim: int) -> float:
-    return math.sqrt(dim)
+class AttentionMixin:
+    def __init__(self, in_dim: int, heads: int):
+        if in_dim % heads != 0:
+            raise ValueError(f"dim {in_dim} not divisible by heads {heads}")
 
+        self.heads = heads
+        self.dim_per_head = in_dim // heads
 
-def uncat_heads(x: Tensor, heads: int) -> Tensor:
-    return rearrange(x, "... (heads dim) -> ... heads dim", heads=heads)
+    @property
+    def attention_scale(self):
+        return math.sqrt(self.dim_per_head)
 
+    def uncat_heads(self, x: Tensor) -> Tensor:
+        return rearrange(x, "... (heads dim) -> ... heads dim", heads=self.heads)
 
-def cat_heads(x: Tensor) -> Tensor:
-    return rearrange(x, "... heads dim -> ... (heads dim)")
+    def cat_heads(self, x: Tensor) -> Tensor:
+        return rearrange(x, "... heads dim -> ... (heads dim)", heads=self.heads)
+
+    def init_weight_layers(self, feature_dim: int) -> tuple[Linear, Linear, Linear]:
+        linQ = Linear(feature_dim, feature_dim)
+        linK = Linear(feature_dim, feature_dim)
+        linV = Linear(feature_dim, feature_dim)
+        return linQ, linK, linV
