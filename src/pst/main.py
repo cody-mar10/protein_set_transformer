4c346e66472e611781202ca5871cc156fabe1b36
cody-mar10 <ccmartin6@wisc.edu>
2024-10-16 13:10:21 -0500

    add finetune mode entrypoint - also actually use the logger for this lib

--- src/pst/main.py
+++ src/pst/main.py
@@ -6,18 +6,19 @@ import lightning as L
 import torch
 from torch.autograd.anomaly_mode import set_detect_anomaly
 
+from pst import _logger as logger
 from pst.embed import embed
 from pst.predict import model_inference
-from pst.training import cv, full
+from pst.training import cv, finetune, full
 from pst.training.tuning import tuning
 from pst.utils.cli import Args, parse_args
-from pst.utils.cli.modes import InferenceMode, TrainingMode, TuningMode
+from pst.utils.cli.modes import FinetuningMode, InferenceMode, TrainingMode, TuningMode
 from pst.utils.download import download
 from pst.utils.graphify import to_graph_format
 from pst.utils.history import update_config_from_history
 
 _SEED = 111
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
 
 def train_main(args: TrainingMode):
@@ -30,6 +31,7 @@ def train_main(args: TrainingMode):
 
 
 def tune_main(args: TuningMode):
+    # this is for hyperparameter tuning
     tuning.tune(args)
 
 
@@ -40,6 +42,12 @@ def test_main(args: Args):
 def predict_main(args: InferenceMode):
     L.seed_everything(_SEED)
     model_inference(args, True, True, False)
+
+
+def finetune_main(args: FinetuningMode):
+    # set lightning trainer dir to --outdir
+    args.trainer.default_root_dir = args.finetuning.outdir
+    finetune.finetune(args)
 
 
 def _check_cpu_accelerator(config: TrainingMode):
@@ -56,8 +64,21 @@ def _validate_accelerator(config: TrainingMode):
         config.trainer.accelerator = "gpu" if torch.cuda.is_available() else "cpu"
 
 
+def _setup_logger():
+    logger.setLevel(logging.INFO)
+    streamhandler = logging.StreamHandler()
+    streamhandler.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        fmt="[%(asctime)s] (%(levelname)s): %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    streamhandler.setFormatter(formatter)
+    logger.addHandler(streamhandler)
+
+
 def main():
     args = parse_args()
+    _setup_logger()
 
     if args.graphify is not None:
         to_graph_format(args.graphify.graphify)
@@ -77,7 +98,11 @@ def main():
     elif args.predict is not None:
         config = args.predict
         fn = predict_main
+    elif args.finetune is not None:
+        config = args.finetune
+        fn = finetune_main
     else:
+        # should not really get here since argparsing should already catch this
         raise RuntimeError("Invalid run mode passed.")
 
     _validate_accelerator(config)
