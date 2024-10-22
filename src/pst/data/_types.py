c20e2b8ea80ccad5bb76151a0e126a85e5fd32b8
cody-mar10 <ccmartin6@wisc.edu>
2024-10-22 12:20:03 -0500

    initial commit for dataset type annotation mixins

---
+++ src/pst/data/_types.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from torch import Tensor
+
+
+class ProteinFeaturesTypeMixin:
+    # shapes should all refer to number of proteins
+    protein_data: Tensor
+    protein_strand: Tensor
+
+
+class ScaffoldFeaturesTypeMixin:
+    # shapes should all refer to number of scaffolds
+    scaffold_ptr: Tensor
+    scaffold_sizes: Tensor
+    scaffold_class_id: Tensor  # TODO: deprecate and move to a registered feature
+    scaffold_weights: Tensor  # TODO: deprecate and move to a registered feature
+    scaffold_edge_indices: list[Tensor]
+    scaffold_label: Tensor
+    scaffold_genome_label: Tensor
+    scaffold_part_of_multiscaffold: Tensor
+
+
+class GenomeFeaturesTypeMixin:
+    # shapes should all refer to number of genomes
+    genome_is_multiscaffold: Tensor
+
+
+FeatureLevel = Literal["node", "graph", "protein", "scaffold", "genome"]
