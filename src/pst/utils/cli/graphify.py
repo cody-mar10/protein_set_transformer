3469749ed65817039d5fd269fd93027bc3a98308
cody-mar10 <ccmartin6@wisc.edu>
2024-10-22 18:44:07 -0500

    inputs can either be a single fasta/ESM file pair or a tsv file of pairs

--- src/pst/utils/cli/graphify.py
+++ src/pst/utils/cli/graphify.py
@@ -7,17 +7,32 @@ from pydantic import BaseModel, Field, FilePath
 
 
 class IOArgs(BaseModel):
-    file: FilePath = Field(..., description="ESM2 protein embeddings in .h5 format")
+    file: Optional[FilePath] = Field(
+        None,
+        description=(
+            "ESM2 protein embeddings in .h5 format. Either this AND --fasta-file must be "
+            "provided OR --multi-input-map must be used."
+        ),
+    )
     loc: str = Field(
         "data",
         description="location of protein embeddings in .h5 file relative to the root node",
     )
-    fasta_file: FilePath = Field(
-        ...,
+    fasta_file: Optional[FilePath] = Field(
+        None,
         description=(
             "The protein FASTA file used to generate the protein embeddings. The embeddings MUST "
             "be in the same order as this file. The protein headers MUST be prodigal format: "
-            "'>scaffold_ptn#'"
+            "'>scaffold_ptn#'. Either this AND --file must be provided OR --multi-input-map must "
+            "be used."
+        ),
+    )
+    multi_input_map: Optional[FilePath] = Field(
+        None,
+        description=(
+            "tab-delimited mapping file for inputting multiple embedding and fasta files. "
+            "Order of columns (no header): (FASTA file, ESM embedding file). Either this or "
+            "--file AND --fasta-file must be provided."
         ),
     )
     output: Optional[Path] = Field(
@@ -34,13 +49,13 @@ class OptionalArgs(BaseModel):
         None,
         description=(
             "tab-delimited strand file for each embedded protein. Must be in the same order as "
-            "the FASTA file. Order of columns: (protein name, strand [-1, 1])."
+            "the FASTA file. Order of columns (no header): (protein name, strand [-1, 1])."
         ),
     )
     scaffold_map_file: Optional[Path] = Field(
         None,
         description=(
-            "tab-delimited mapping file for multi-scaffold viruses Order of columns: "
+            "tab-delimited mapping file for multi-scaffold viruses Order of columns (no header): "
             "(scaffold name, genome name). See FASTA file description to understand how we "
             "define the scaffold name."
         ),
