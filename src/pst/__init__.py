8883611dd240cf7930d4eb2b46848f79560bc684
cody-mar10 <ccmartin6@wisc.edu>
2024-11-07 15:51:01 -0600

    get base classes from correct path - also added `__all__`

--- src/pst/__init__.py
+++ src/pst/__init__.py
@@ -7,13 +7,27 @@ from pst.data.dataset import GenomeDataset
 from pst.data.graph import GenomeGraph
 from pst.data.loader import GenomeDataLoader
 from pst.data.modules import DataConfig, GenomeDataModule
+from pst.nn.base import BaseProteinSetTransformer, BaseProteinSetTransformerEncoder
 from pst.nn.config import BaseLossConfig, BaseModelConfig, ModelConfig
-from pst.nn.modules import (
-    BaseProteinSetTransformer,
-    BaseProteinSetTransformerEncoder,
-    ProteinSetTransformer,
-    ProteinSetTransformerEncoder,
-)
-from pst.typing import GenomeGraphBatch
+from pst.nn.modules import ProteinSetTransformer, ProteinSetTransformerEncoder
+from pst.typing import GenomeGraphBatch, MaskedGenomeGraphBatch
+
+__all__ = [
+    "GenomeDataset",
+    "GenomeGraph",
+    "GenomeDataLoader",
+    "DataConfig",
+    "GenomeDataModule",
+    "BaseProteinSetTransformer",
+    "BaseProteinSetTransformerEncoder",
+    "BaseLossConfig",
+    "BaseModelConfig",
+    "ModelConfig",
+    "ProteinSetTransformer",
+    "ProteinSetTransformerEncoder",
+    "GenomeGraphBatch",
+    "MaskedGenomeGraphBatch",
+]
 
 _logger = logging.getLogger(__name__)
+
