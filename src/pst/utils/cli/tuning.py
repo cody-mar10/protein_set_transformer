6364d2341d8b575077dc88bb8ef2065c86a162c5
cody-mar10 <ccmartin6@wisc.edu>
2024-12-09 17:18:55 -0600

    convert all configs from `pydantic.BaseModel` to `dataclasses.dataclass` - this is to be interoperable with `jsonargparse` - this also required many changes to the entry points for the logic that is nearly identical - CHANGES:   - made `config` as a required field (sort of moved over from `ExperimentArgs`

--- src/pst/utils/cli/tuning.py
+++ src/pst/utils/cli/tuning.py
@@ -1,27 +1,37 @@
-from __future__ import annotations
-
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from jsonargparse.typing import NonNegativeInt, PositiveInt
+
+from pst.utils.dataclass_utils import DataclassValidatorMixin, validated_field
 
 
-class TuningArgs(BaseModel):
-    n_trials: int = Field(1, description="number of tuning trials to run", gt=0)
-    prune: bool = Field(
-        True, description="whether to allow pruning of unpromising trials"
-    )
-    parallel: bool = Field(
-        False,
-        description="NOT IMPL: whether to run tuning trials in parallel if multiple GPUs are available",  # noqa: E501
-    )
-    tuning_dir: Optional[Path] = Field(
-        None,
-        description="tuning directory path that stores optuna SQL history databases",
-    )
-    pruning_warmup_steps: int = Field(
-        3, description="number of epochs before pruning can start"
-    )
-    pruning_warmup_trials: int = Field(
-        1, description="number of trials allowed to complete before pruning can start"
-    )
+@dataclass
+class TuningArgs(DataclassValidatorMixin):
+    """TUNING
+
+    Hyperparameter tuning configuration.
+    """
+
+    # TODO: this makes most sense here.... 
+    config: Path
+    """config toml file to specify which hyperparameters to tune with optuna"""
+
+    n_trials: int = validated_field(1, validator=PositiveInt)
+    """number of tuning trials to run"""
+
+    prune: bool = True
+    """whether to allow pruning of unpromising trials"""
+
+    parallel: bool = False
+    """NOT IMPL: whether to run tuning trials in parallel if multiple GPUs are available"""
+
+    tuning_dir: Optional[Path] = None
+    """tuning directory path that stores optuna SQL history databases"""
+
+    pruning_warmup_steps: int = validated_field(3, validator=PositiveInt)
+    """number of epochs before pruning can start"""
+
+    pruning_warmup_trials: int = validated_field(1, validator=NonNegativeInt)
+    """number of trials allowed to complete before pruning can start"""
