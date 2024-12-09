c4e61478d6309e96d43d416674428b1c06871294
cody-mar10 <ccmartin6@wisc.edu>
2024-12-09 17:44:51 -0600

    update with new dataclass configs - only allow CV datamodules

--- src/pst/training/cv.py
+++ src/pst/training/cv.py
@@ -1,41 +1,62 @@
-from __future__ import annotations
-
 import logging
 
 import lightning_cv as lcv
 
-from pst.data.modules import GenomeDataModule
+from pst.data.config import CrossValDataConfig
+from pst.data.modules import CrossValGenomeDataModule
+from pst.nn.base import BaseModelTypes
+from pst.nn.config import BaseModelConfig
+from pst.nn.modules import ProteinSetTransformer
 from pst.training.utils.cv import init_trainer_config
-from pst.training.utils.dim import check_feature_dim
-from pst.utils.auto import auto_resolve_model_type
-from pst.utils.cli.modes import TrainingMode
+from pst.training.utils.pst import _add_group_weights
+from pst.utils.cli.experiment import ExperimentArgs
+from pst.utils.cli.trainer import TrainerArgs
 
 logger = logging.getLogger(__name__)
 
 
-def train_with_cross_validation(config: TrainingMode):
+def train_with_cross_validation(
+    model_type: BaseModelTypes,
+    model_cfg: BaseModelConfig,
+    data: CrossValDataConfig,
+    trainer_cfg: TrainerArgs,
+    experiment: ExperimentArgs,
+):
     logger.info("Training with cross validation.")
 
     trainer_config = init_trainer_config(
-        config,
+        model_cfg,
+        experiment,
+        trainer_cfg,
         checkpoint=True,
         early_stopping=True,
         timer=True,
     )
 
-    datamodule = GenomeDataModule(config.data)
-    check_feature_dim(config)
+    datamodule = CrossValGenomeDataModule(data)
 
-    model_type = auto_resolve_model_type(config.experiment.pst_model_type)
+    # backwards compatibility with PST that computes group weights
+    if model_type is ProteinSetTransformer:
+        _add_group_weights(datamodule)
 
     trainer = lcv.CrossValidationTrainer(
         model_type=model_type,  # type: ignore
         config=trainer_config,
     )
 
-    # TODO: need to figure out how to update positional embedding max size if needed
-    # simplest solution is to just pass that as an arg to the model thru the config...
+    actual_max_size = datamodule.dataset.max_size
+    expected_max_size = model_cfg.max_proteins
+
+    if actual_max_size > expected_max_size:
+        logger.warning(
+            f"Dataset max size ({actual_max_size}) is greater than model max proteins "
+            f"({expected_max_size}). Since a model hasn't been created yet, the model max "
+            "proteins will be set to the dataset max size."
+        )
+
+        model_cfg.max_proteins = actual_max_size
+
     trainer.train_with_cross_validation(
         datamodule=datamodule,
-        model_config=config.model,
+        model_config=model_cfg,
     )
