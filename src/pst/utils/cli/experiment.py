1df2410f28478fadce081558e31d8149008bc9f2
cody-mar10 <ccmartin6@wisc.edu>
2024-12-09 17:18:14 -0600

    convert all configs from `pydantic.BaseModel` to `dataclasses.dataclass` - this is to be interoperable with `jsonargparse` - this also required many changes to the entry points for the logic that is nearly identical - CHANGES:   - moved several `lightning.Trainer` specific/adjacent fields from `ExperimentArgs` to an `extra` field of the `TrainerArgs`   - merged the `from_study` and `from_history` args into a single field called `best_trial` since the processing logic was the same

--- src/pst/utils/cli/experiment.py
+++ src/pst/utils/cli/experiment.py
@@ -1,73 +1,33 @@
-from __future__ import annotations
-
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Literal, Optional
+from typing import Optional
 
-from pydantic import BaseModel, Field
-
-AnnealingOpts = Literal["linear", "cos"]
-ModelType = Literal[
-    "ProteinSetTransformer", "MLMProteinSetTransformer", "ProteinSetTransformerEncoder"
-]
+from pst.utils.dataclass_utils import DataclassValidatorMixin
 
 
-class ExperimentArgs(BaseModel):
-    # this has to stay here so it is always available even when not tuning
-    config: Optional[Path] = Field(
-        None,
-        description="config toml file to specify which hyperparameters to tune with optuna",  # noqa: E501
-    )
-    name: str = Field(
-        "exp0", description="experiment name in logging directory during training"
-    )
-    patience: int = Field(
-        5, description="early stopping patience, see lightning description", gt=0
-    )
-    save_top_k: int = Field(1, description="save the best k models", gt=0)
-    ### SWA args
-    swa: bool = Field(False, description="use stochastic weight averaging")
-    swa_epoch_start: int | float = Field(
-        0.8,
-        description=(
-            "when to start SWA if enabled. Integers are an epoch number, "
-            "while floats in range (0.0, 1.0) are a fraction of max epochs. "
-        ),
-    )
-    annealing_epochs: int = Field(
-        10,
-        description="number of epochs to anneal learning rate during swa if enabled",
-        gt=0,
-    )
-    annealing_strategy: AnnealingOpts = Field(
-        "linear", description="annealing strategy using during swa if enabled"
-    )
-    ### related to tuning -> get params from previous runs
-    from_study: Optional[Path] = Field(
-        None, description="load hyperparameters from previous tuning study"
-    )
-    from_json: Optional[Path] = Field(
-        None, description="load hyperparameters from json file"
-    )
-    log_gradients: bool = Field(False, description="debug mode to log gradients")
-    detect_anomaly: bool = Field(
-        False, description="debug mode to detect gradient anomalies"
-    )
-    pst_model_type: ModelType = Field(
-        "ProteinSetTransformer",
-        description=(
-            "model type to train. `ProteinSetTransformer` is described in the manuscript and is "
-            "trained with triplet loss and pointswap augmentation. `MLMProteinSetTransformer` "
-            "is trained using masked language modeling loss with positive sampling. "
-            "`ProteinSetTransformerEncoder` is an encoder-only variant of "
-            "`ProteinSetTransformer` that trains only on protein embeddings with triplet loss "
-            "at the protein level."
-        ),
-    )
-    min_delta: float = Field(
-        0.05,
-        description=(
-            "loss has to improve by this much before --patience epochs otherwise training will "
-            "stop early"
-        ),
-        ge=0.0,
-    )
+@dataclass
+class ExperimentArgs(DataclassValidatorMixin):
+    """EXPERIMENT"""
+
+    name: str = "exp0"
+    """experiment name in logging directory during training"""
+
+    best_trial: Optional[Path] = None
+    """load hyperparameters from a previous tuning run. Acceptable inputs include:
+    - (RECOMMENDED) an optuna sqlite database that holds the entire history of a tuning study
+    - a json file that holds the best hyperparameters from a tuning study produced by `lightning_cv`
+        - Otherwise, this can be any json file that has a key `hparams` whose value is a flat dictionary
+        of hyperparameter names and sampled values
+    - a yaml file that holds the best hyperparameters from a tuning study. Note: the yaml file
+    can also be a config file produced by `jsonargparse` that holds the best hyperparameters.
+    NOTE: this is just a convenience feature to load all hyperparameters from a previous tuning run,
+    but they can also be entered individually at the command line.
+    """
+
+    config: Optional[Path] = None
+    """Tuning trial hyperparameter config file that was used to define the sampling space.
+    This is the same file that should've been passed to `--tuning.config` during hyperparameter
+    tuning. This is needed if any hyperparameters were sampled from a categorical distribution
+    whose values get mapped to other inputs. Ex: Sampling a model single `complexity` 
+    hyperparameter that adjusts the number of layers, attention heads, embedding dim, etc.
+    """
--- src/pst/utils/cli/trainer.py
+++ src/pst/utils/cli/trainer.py
@@ -1,16 +1,41 @@
-from __future__ import annotations
-
+from dataclasses import dataclass, field
 from datetime import timedelta
 from enum import Enum
 from pathlib import Path
 from typing import Literal, Optional
 
-from pydantic import BaseModel, Field, field_validator
+import torch
+from jsonargparse.typing import (
+    NonNegativeInt,
+    OpenUnitInterval,
+    PositiveFloat,
+    PositiveInt,
+)
 
-AcceleratorOpts = Literal["cpu", "gpu", "tpu", "auto"]
-PrecisionOpts = Literal["16-mixed", "bf16-mixed", "32"]
-StrategyOpts = Literal["ddp", "ddp_spawn", "ddp_notebook", "fsdp", "auto"]
+from pst.utils.dataclass_utils import DataclassValidatorMixin, validated_field
+
 GradClipAlgOpts = Literal["norm", "value"]
+
+
+class AcceleratorOpts(str, Enum):
+    cpu = "cpu"
+    gpu = "gpu"
+    tpu = "tpu"
+    auto = "auto"
+
+
+class PrecisionOpts(str, Enum):
+    bf16 = "bf16-mixed"
+    fp16 = "16-mixed"
+    fp32 = "32"
+
+
+class StrategyOpts(str, Enum):
+    ddp = "ddp"
+    ddp_spawn = "ddp_spawn"
+    ddp_notebook = "ddp_notebook"
+    fsdp = "fsdp"
+    auto = "auto"
 
 
 class MaxTimeOpts(Enum):
@@ -23,55 +48,121 @@ class MaxTimeOpts(Enum):
         return self.name
 
 
-class TrainerArgs(BaseModel):
-    devices: int = Field(
-        1,
-        description="number of accelerator devices to use. For CPUs, this sets the total thread usage.",  # noqa: E501
-    )
-    accelerator: AcceleratorOpts = Field("auto", description="accelerator to use")
-    default_root_dir: Path = Field(
-        Path("lightning_root"),
-        description="lightning root dir for checkpointing and logging",
-    )
-    max_epochs: int = Field(1000, description="max number of training epochs", gt=0)
-    precision: PrecisionOpts = Field("16-mixed", description="floating point precision")
-    strategy: StrategyOpts = Field("ddp", description="parallelized training strategy")
-    gradient_clip_algorithm: Optional[GradClipAlgOpts] = Field(
-        "norm", description="optional gradient clipping procedure"
-    )
-    gradient_clip_val: Optional[float] = Field(
-        0.5, description="optional value if clipping gradients"
-    )
-    max_time: MaxTimeOpts = Field(
-        MaxTimeOpts.none,
-        description=(
-            "maximum allowed training time"
-            "[choices: short=12h, medium=1d, long=7d, none=no limit]"
-        ),
-    )
-    limit_train_batches: Optional[int | float] = Field(
-        None,
-        description=(
-            "optional limit to number of training batches. An integer means "
-            "train with that number of training batches, while a float between "
-            "(0.0, 1.0] means that fraction of the training data is used)"
-        ),
-    )
-    limit_val_batches: Optional[int | float] = Field(
-        None,
-        description=(
-            "optional limit to number of val batches. An integer means "
-            "validate with that number of val batches, while a float between "
-            "(0.0, 1.0] means that fraction of the val data is used)"
-        ),
-    )
-    accumulate_grad_batches: int = Field(
-        1,
-        description="number of batches to accumulate gradients before the optimizer steps",
-    )
+class SWAAnnealingOpts(str, Enum):
+    linear = "linear"
+    cos = "cos"
 
-    @field_validator("max_time", mode="before")
-    def _convert(cls, value: str | MaxTimeOpts) -> MaxTimeOpts:
-        if isinstance(value, str):
-            return MaxTimeOpts[value]
-        return value
+
+@dataclass
+class ExtraTrainerArgs(DataclassValidatorMixin):
+    """EXTRA TRAINER ARGS"""
+
+    # most of this is being moved from Experiment since they really apply to trainers
+
+    patience: int = validated_field(5, validator=PositiveInt)
+    """early stopping patience, see lightning description"""
+
+    save_top_k: int = validated_field(1, validator=NonNegativeInt)
+    """save the best k models"""
+
+    swa: bool = False
+    """use stochastic weight averaging"""
+
+    swa_epoch_start: float = validated_field(0.8, validator=OpenUnitInterval)
+    """when to start SWA if enabled. Integers are an epoch number, while floats in range (0.0, 1.0) are a fraction of max epochs."""
+
+    annealing_epochs: int = validated_field(10, validator=PositiveInt)
+    """number of epochs to anneal learning rate during swa if enabled"""
+
+    annealing_strategy: SWAAnnealingOpts = SWAAnnealingOpts.linear
+    """annealing strategy using during swa if enabled"""
+
+    min_delta: float = validated_field(0.05, validator=PositiveFloat)
+    """minimum change in monitored quantity to qualify as an improvement. There must be an
+    improvement in at least --patience epochs before an EarlyStopping callback is checked
+    """
+
+
+@dataclass
+class TrainerArgs(DataclassValidatorMixin):
+    """TRAINER"""
+
+    devices: int = validated_field(1, validator=PositiveInt)
+    """number of accelerator devices to use. For CPUs, this sets the total thread usage."""
+
+    accelerator: AcceleratorOpts = AcceleratorOpts.auto
+    """accelerator to use"""
+
+    default_root_dir: Path = Path("lightning_root")
+    """lightning root dir for checkpointing and logging"""
+
+    max_epochs: int = validated_field(1000, validator=PositiveInt)
+    """max number of training epochs"""
+
+    precision: PrecisionOpts = PrecisionOpts.bf16
+    """floating point precision"""
+
+    strategy: StrategyOpts = StrategyOpts.auto
+    """parallelized training strategy"""
+
+    gradient_clip_algorithm: Optional[GradClipAlgOpts] = "norm"
+    """optional gradient clipping procedure"""
+
+    gradient_clip_val: Optional[float] = 0.5
+    """optional value if clipping gradients"""
+
+    max_time: MaxTimeOpts = MaxTimeOpts.none
+    """maximum allowed training time"""
+
+    limit_train_batches: Optional[int | float] = None
+    """optional limit to number of training batches. An integer means train with that number of
+    training batches, while a float between (0.0, 1.0] means that fraction of the training data
+    is used"""
+
+    limit_val_batches: Optional[int | float] = None
+    """optional limit to number of val batches. An integer means validate with that number of val
+    batches, while a float between (0.0, 1.0] means that fraction of the val data is used"""
+
+    accumulate_grad_batches: int = validated_field(1, validator=PositiveInt)
+    """number of batches to accumulate gradients over"""
+
+    extra: ExtraTrainerArgs = field(default_factory=ExtraTrainerArgs)
+    """EXTRA TRAINER ARGS"""
+
+    def _validate_limit_batches(
+        self, attr: Literal["limit_train_batches", "limit_val_batches"]
+    ):
+        value = getattr(self, attr)
+        if value is not None:
+            if isinstance(value, int) and value <= 0:
+                raise ValueError(f"{attr} must be >= 0 if an integer")
+            elif isinstance(value, float) and (value <= 0 or value > 1):
+                raise ValueError(f"{attr} must be in (0.0, 1.0] if a float")
+
+    def _validate_accelerator(self):
+        if self.accelerator == "auto":
+            self.accelerator = (
+                AcceleratorOpts.gpu
+                if torch.cuda.is_available()
+                else AcceleratorOpts.cpu
+            )
+
+    def _check_cpu_accelerator(self):
+        if self.accelerator == "cpu":
+            threads = self.devices
+            self.precision = PrecisionOpts.fp32
+            torch.set_num_threads(threads)
+            self.devices = 1
+            self.strategy = StrategyOpts.auto
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        for attr in ["limit_train_batches", "limit_val_batches"]:
+            self._validate_limit_batches(attr)  # type: ignore
+
+        if self.gradient_clip_val is not None and self.gradient_clip_val < 0:
+            raise ValueError("gradient_clip_val must be >= 0 if specified")
+
+        self._validate_accelerator()
+        self._check_cpu_accelerator()
