04c4ee795ff4945161652479a28b1e31ad8557d8
cody-mar10 <ccmartin6@wisc.edu>
2025-01-20 11:02:06 -0600

    custom validation and conversion for `validation` file or strategy - import `attrs` utilites from new submodule - use `enum_field` with custom converter and validator - this all plays nicer with automatic de/ser with `cattrs`

--- src/pst/data/config.py
+++ src/pst/data/config.py
@@ -1,10 +1,9 @@
-from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
-from typing import Literal, Optional
+from typing import Literal, Union
 from warnings import warn
 
-from jsonargparse.typing import NonNegativeInt, PositiveInt, restricted_number_type
+from attrs import define, field, validators
 from lightning_cv.split import ImbalancedLeaveOneGroupOut
 from sklearn.model_selection import (
     GroupKFold,
@@ -16,7 +15,14 @@ from sklearn.model_selection import (
 from pst.data.dataset import _SENTINEL_FRAGMENT_SIZE
 from pst.data.loader import GenomeDataLoader, ScaffoldDataLoader
 from pst.typing import EdgeIndexStrategy
-from pst.utils.dataclass_utils import DataclassValidatorMixin, validated_field
+from pst.utils.attrs.converters import enum_field
+from pst.utils.attrs.dataclass_utils import AttrsDataclassUtilitiesMixin
+from pst.utils.attrs.validators import (
+    file_exists,
+    non_negative_int,
+    optional_positive_int,
+    positive_int,
+)
 
 
 class CrossValidationType(str, Enum):
@@ -55,24 +61,57 @@ class CVStrategies(Enum):
         return {cls.StratifiedGroupKFold}
 
 
-ChunkSize = restricted_number_type(
-    "ChunkSize",
-    int,
-    [(">=", 10), ("<=", 50)],
-    docstring="Chunk size must be in [10, 50]",
+chunk_size_validator = validators.and_(
+    validators.instance_of(int),
+    validators.ge(10),
+    validators.le(50),
 )
 
-OptionalPositiveInt = restricted_number_type(
-    "OptionalPositiveInt",
-    int,
-    [(">", 0), ("==", -1)],
-    join="or",
-    docstring="Positive integer or -1 (representing optional value)",
-)
+# must be defined in this order to preferentially receive random first
+_OptionalValidationArg = Union[Literal["random"], Path, None]
 
 
-@dataclass
-class DataConfig(DataclassValidatorMixin):
+def _optional_validation_arg_validator(instance, field, value: _OptionalValidationArg):
+    if isinstance(value, Path):
+        if not value.exists():
+            # TODO: not sure if it also just be a ValueError for consistency
+            raise FileNotFoundError(f"File not found: {value}")
+        return
+
+    if isinstance(value, str):
+        if value != "random":
+            raise ValueError(
+                f"The only allowed string that is not a filepath is 'random'. Received: {value}"
+            )
+        return
+
+    if value is not None:
+        raise ValueError(
+            f"Value must be 'random', an existing filepath, or None. Received: {value}"
+        )
+
+
+def _optional_validation_arg_converter(
+    value: Union[Path, str, None],
+) -> _OptionalValidationArg:
+    if value is None:
+        return value
+
+    if isinstance(value, str):
+        if value == "random":
+            return value
+        return Path(value)
+
+    try:
+        # convert to str then to Path
+        # if it is not an existing file, the validator will raise an error
+        return Path(str(value))
+    except TypeError as e:
+        raise ValueError(f"Could not convert argument: {value}") from e
+
+
+@define
+class DataConfig(AttrsDataclassUtilitiesMixin):
     """DATA
 
     Data configuration to load a GenomeDataModule that does not implement cross validation.
@@ -109,39 +148,46 @@ class DataConfig(DataclassValidatorMixin):
         dataloader (DataLoaderType): dataloader type for loading minibatches of data.
     """
 
-    file: Path
+    file: Path = field(validator=file_exists, converter=Path)
     """input protein embeddings file in `pst graphify` .h5 file format. See the wiki for more 
     information if manually creating this file. Otherwise, the `pst graphify` workflow will 
     create this file correctly."""
 
-    validation: Optional[Literal["random"] | Path] = None
+    validation: _OptionalValidationArg = field(
+        default=None,
+        validator=_optional_validation_arg_validator,
+        converter=_optional_validation_arg_converter,
+    )
     """How to form a validation set if specified. If not provided, no validation will occur, so
     when training a model, the model will be trained on the entire dataset provided to --file.
     If the input is a file path, this will be loaded as an independent dataset for validation.
     Otherwise, the input dataset to --file will be randomly split at an 80:20 ratio"""
 
-    batch_size: int = validated_field(default=32, validator=PositiveInt)
+    batch_size: int = field(default=32, validator=positive_int)
     """batch size in number of genomes"""
 
     pin_memory: bool = True
     """whether to pin memory onto a CUDA GPU or not"""
 
-    num_workers: int = validated_field(default=0, validator=NonNegativeInt)
+    num_workers: int = field(default=0, validator=non_negative_int)
     """additional cpu workers to load data"""
 
-    edge_strategy: EdgeIndexStrategy = "chunked"
+    edge_strategy: EdgeIndexStrategy = enum_field(
+        enum_cls=EdgeIndexStrategy, default=EdgeIndexStrategy.chunked
+    )
     """strategy to create 'edges' between protein nodes in a genome graph. chunked = split 
     genomes in --chunk-size chunks. sparse = remove interactions longer than --threshold. full 
     = fully connected graph like a regular transformer."""
 
-    chunk_size: int = validated_field(default=30, validator=ChunkSize)
+    chunk_size: int = field(default=30, validator=chunk_size_validator)
     """size of sub-chunks to break genomes into if using --edge_strategy chunked. This is the 
     range of protein-protein neighborhoods within a contiguous scaffold. This is different from
     --fragment-size, which controls artificially fragmenting scaffolds before protein-protein 
     neighborhoods are calculated. Protein-protein neighborhoods are only calculated within a 
     contiguous scaffold (happens AFTER --fragment-size fragmenting)"""
 
-    threshold: int = validated_field(default=-1, validator=OptionalPositiveInt)
+    # TODO: I think this is really called 'context_size' in ML terms
+    threshold: int = field(default=-1, validator=optional_positive_int)
     """range of protein interactions if using --edge_strategy [chunked|sparse]. Default is
     for no thresholding to occur, meaning that all proteins in the same scaffold/chunk will
     be connected in the genome graph.
@@ -150,13 +196,15 @@ class DataConfig(DataclassValidatorMixin):
     log_inverse: bool = False
     """take the log of inverse class freqs as weights"""
 
-    fragment_size: int = validated_field(
-        default=_SENTINEL_FRAGMENT_SIZE, validator=OptionalPositiveInt
+    fragment_size: int = field(
+        default=_SENTINEL_FRAGMENT_SIZE, validator=optional_positive_int
     )
     """artificially break scaffolds into fragments that have no more than this many proteins
     Default is no fragmentation."""
 
-    dataloader: DataLoaderType = DataLoaderType.scaffold
+    dataloader: DataLoaderType = enum_field(
+        enum_cls=DataLoaderType, default=DataLoaderType.scaffold
+    )
     """dataloader type for loading minibatches of data
     - `genome` will load the proteins from all scaffolds that belong to each genome, which is
     useful when there are multiscaffold genomes
@@ -164,24 +212,8 @@ class DataConfig(DataclassValidatorMixin):
     source genome has other scaffolds
     """
 
-    def __post_init__(self):
-        super().__post_init__()
 
-        if not isinstance(self.file, Path):
-            self.file = Path(self.file)
-
-        if not self.file.exists():
-            raise FileNotFoundError(f"File not found: {self.file}")
-
-        if self.validation is not None and self.validation != "random":
-            if not isinstance(self.validation, Path):
-                self.validation = Path(self.validation)
-
-            if not self.validation.exists():
-                raise FileNotFoundError(f"Validation file not found: {self.validation}")
-
-
-@dataclass
+@define
 class CrossValDataConfig(DataConfig):
     """CROSS VAL DATA
 
@@ -196,10 +228,14 @@ class CrossValDataConfig(DataConfig):
             be registered as a feature in the .h5 file.
     """
 
-    cv_strategy: CVStrategies = CVStrategies.ImbalancedLeaveOneGroupOut
+    cv_strategy: CVStrategies = enum_field(
+        enum_cls=CVStrategies, default=CVStrategies.ImbalancedLeaveOneGroupOut
+    )
     """cross-validation strategy"""
 
-    cv_type: CrossValidationType = CrossValidationType.group
+    cv_type: CrossValidationType = enum_field(
+        enum_cls=CrossValidationType, default=CrossValidationType.group
+    )
     """what kind of label to split on for cross-validation"""
 
     cv_var_name: str = "group_id"
@@ -207,9 +243,7 @@ class CrossValDataConfig(DataConfig):
     feature in the .h5 file. If using --cv_type both, this should be a comma-separated list
     in the order of label,group. Ex: --cv_type both --cv_var_name 'y,group_id'"""
 
-    def __post_init__(self):
-        super().__post_init__()
-
+    def __attrs_post_init__(self):
         if self.validation is not None:
             warn(
                 "validation is not used for cross-validation data modules",
