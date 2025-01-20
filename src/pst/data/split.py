11243fc6c6603374e214f8481cf4782972db9b45
cody-mar10 <ccmartin6@wisc.edu>
2025-01-20 11:06:54 -0600

    add `_accumulate` impl since it is not always available from `torch`

--- src/pst/data/split.py
+++ src/pst/data/split.py
@@ -1,13 +1,30 @@
 import math
 import warnings
-from typing import Literal, Optional, Sequence, cast
+from operator import add
+from typing import Callable, Iterable, Iterator, Literal, Optional, Sequence, cast
 
 from torch import Generator, default_generator, randperm
-from torch._utils import _accumulate
 from torch.utils.data import Subset
 from torch.utils.data import random_split as scaffold_random_split
 
 from pst.data.dataset import GenomeDataset, SubsetGenomeDataset
+from pst.typing import _T
+
+
+# Taken from python 3.5 docs
+def _accumulate(iterable: Iterable[_T], fn: Callable[[_T, _T], _T] = add) -> Iterator[_T]:
+    "Return running totals"
+    # _accumulate([1,2,3,4,5]) --> 1 3 6 10 15
+    # _accumulate([1,2,3,4,5], operator.mul) --> 1 2 6 24 120
+    it = iter(iterable)
+    try:
+        total = next(it)
+    except StopIteration:
+        return
+    yield total
+    for element in it:
+        total = fn(total, element)
+        yield total
 
 
 def genome_random_split(
@@ -15,7 +32,6 @@ def genome_random_split(
     lengths: Sequence[int | float],
     generator: Optional[Generator] = default_generator,
 ) -> list[SubsetGenomeDataset]:
-
     # this is the same implementation as pytorch's random_split except using the
     # GenomeDataset.num_genomes for dataset size
     dataset_len = dataset.num_genomes
@@ -37,15 +53,12 @@ def genome_random_split(
         for i, length in enumerate(lengths):
             if length == 0:
                 warnings.warn(
-                    f"Length of split at index {i} is 0. "
-                    f"This might result in an empty dataset."
+                    f"Length of split at index {i} is 0. This might result in an empty dataset."
                 )
 
     # Cannot verify that dataset is Sized
     if sum(lengths) != dataset_len:  # type: ignore[arg-type]
-        raise ValueError(
-            "Sum of input lengths does not equal the length of the input dataset!"
-        )
+        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
 
     indices = randperm(sum(lengths), generator=generator).tolist()  # type: ignore[arg-type, call-overload]
     splits = [
