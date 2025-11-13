5c7c17f4e073273a175a6f7d94199afd49e4b30a
cody-mar10 <ccmartin6@wisc.edu>
2025-11-13 10:55:46 -0600

    add ordering dict to keep track of original seq order - this prevents a bug with embeddings being returned out of order - also create custom `FastaBatchedDataset` subclass to remove stop codons (`*`)

--- src/pst/embed/data.py
+++ src/pst/embed/data.py
@@ -5,12 +5,55 @@ import torch
 from torch.utils.data import Dataset
 
 from pst.embed.model import BatchType
+from pst.typing import FilePath
+
+
+class FastaBatchedDataset(esm.FastaBatchedDataset):
+    @classmethod
+    def from_file(cls, fasta_file: FilePath):
+        sequence_labels: list[str] = []
+        sequence_strs: list[str] = []
+        cur_seq_label: str | None = None
+        buf: list[str] = []
+
+        def _flush_current_seq():
+            nonlocal cur_seq_label, buf
+            if cur_seq_label is None:
+                return
+            sequence_labels.append(cur_seq_label)
+
+            # remove stop codons
+            sequence = "".join(buf).replace("*", "")
+            sequence_strs.append(sequence)
+            cur_seq_label = None
+            buf = []
+
+        with open(fasta_file, "r") as infile:
+            for line_idx, line in enumerate(infile):
+                if line.startswith(">"):  # label line
+                    _flush_current_seq()
+
+                    # this is the header
+                    line = line[1:].strip()
+                    if len(line) > 0:
+                        cur_seq_label = line
+                    else:
+                        cur_seq_label = f"seqnum{line_idx:09d}"
+                else:  # sequence line
+                    buf.append(line.strip())
+
+        _flush_current_seq()
+
+        assert len(set(sequence_labels)) == len(sequence_labels), (
+            "Found duplicate sequence labels"
+        )
+
+        return cls(sequence_labels, sequence_strs)
 
 
 class SequenceDataset(Dataset):
     """Sequence dataset that pre-batches input sequences by number of tokens"""
 
-    # TODO: should create custom FastaBatchedDataset to read seqs and remove stop codons
     def __init__(
         self, data: esm.FastaBatchedDataset, alphabet: esm.Alphabet, batch_size: int
     ) -> None:
@@ -23,11 +66,30 @@ class SequenceDataset(Dataset):
             batch_size (int): size in number of tokens (ie number of residues)
         """
         self.data = data
+
+        self.seq2order: dict[str, int] = {
+            seq: i for i, seq in enumerate(self.data.sequence_labels)
+        }
+
         # this is in number of tokens, not individual seqs
         self.batch_size = batch_size
 
         self.batch_converter = alphabet.get_batch_converter()
         self.batch_indices = data.get_batch_indices(batch_size)
+
+    @classmethod
+    def from_file(
+        cls, fasta_file: FilePath, alphabet: esm.Alphabet, batch_size: int
+    ):
+        return cls(
+            data=FastaBatchedDataset.from_file(fasta_file),
+            alphabet=alphabet,
+            batch_size=batch_size,
+        )
+
+    @property
+    def num_sequences(self) -> int:
+        return len(self.data.sequence_labels)
 
     def __len__(self) -> int:
         return len(self.batch_indices)
