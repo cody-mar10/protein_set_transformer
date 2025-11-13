f73d6ec97700bb1da16a351d10f8cb90cf783799
cody-mar10 <ccmartin6@wisc.edu>
2025-11-13 10:56:23 -0600

    use dataset's original seq ordering to write embeddings in correct order - also write PST version to file

--- src/pst/embed/writer.py
+++ src/pst/embed/writer.py
@@ -1,3 +1,4 @@
+import importlib.metadata
 from pathlib import Path
 from typing import Literal, Optional, Sequence, cast
 
@@ -10,6 +11,12 @@ from numpy.typing import NDArray
 
 from pst.embed.data import SequenceDataset
 from pst.embed.model import ESM2, BatchType
+
+PST_VERSION = importlib.metadata.version("ptn-set-transformer")
+
+
+class Version(tb.IsDescription):
+    version = tb.StringCol(itemsize=20)  # type: ignore
 
 
 class PredictionWriter(BasePredictionWriter):
@@ -25,17 +32,33 @@ class PredictionWriter(BasePredictionWriter):
         self.compression = tb.Filters(complevel=4, complib="blosc:lz4")
         self._output = self.outdir.joinpath("predictions.h5")
         self._file = tb.File(self._output, "w")
-        self.output = self._file.create_earray(
+
+        self._write_version_table()
+
+        # create an empty array on disk
+        # then just add the embeddings to the appropriate place as they are computed
+        # (which happens potentially out of order)
+        self.output = self._file.create_carray(
             self._file.root,
             "data",
             atom=tb.Float32Atom(),
-            shape=(0, model.model.embed_dim),
-            expectedrows=len(dataset),
+            shape=(dataset.num_sequences, model.model.embed_dim),
             filters=self.compression,
         )
 
-    # TODO: should probably write the sequence names...
-    # but for now just require users to keep track of the source fasta file
+        self.seq2order = dataset.seq2order
+
+    def _write_version_table(self):
+        table = self._file.create_table(
+            where=self._file.root,
+            name="_version",
+            description=Version,  # type: ignore
+        )
+
+        table.row["version"] = PST_VERSION
+        table.row.append()
+        table.flush()
+
     def write_on_batch_end(
         self,
         trainer: L.Trainer,
@@ -46,10 +69,18 @@ class PredictionWriter(BasePredictionWriter):
         batch_idx: int,
         dataloader_idx: int,
     ):
-        data: NDArray[np.float32] = predictions.detach().cpu().numpy()
-        self.output.append(data)
+        seqnames, seqs, tokens = batch
 
-    def on_predict_end(self, trainer: L.Trainer, pl_module: L.LightningModule) -> None:
+        sequence_order: NDArray[np.int64] = np.array(
+            [self.seq2order[name] for name in seqnames]
+        )
+
+        data: NDArray[np.float32] = predictions.detach().cpu().numpy()
+        self.output[sequence_order, :] = data  # type: ignore
+
+    def on_predict_end(
+        self, trainer: L.Trainer, pl_module: L.LightningModule
+    ) -> None:
         pl_module = cast(ESM2, pl_module)
         self._file.close()
         model_name = pl_module.LAYERS_TO_MODELNAME[pl_module.repr_layers]
